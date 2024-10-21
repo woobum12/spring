6388cbd56b89dba3b1f5335ec977297e1a8d304e
woobum12 <woobum1216@gmail.com>
2024-10-21 22:26:46 +0900

    1021~~

---
+++ spring09/hrd.wjb/hrdHome/src/main/java/com/wjb/HrdHomeApplication.java
@@ -0,0 +1,35 @@
+package com.wjb;
+
+
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HrdHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrdHomeApplication.class, args);
+	}
+	
+	//DB연결 테스트
+	private static final Logger logger = LoggerFactory.getLogger(HrdHomeApplication.class);
+	
+	@Bean
+	CommandLineRunner testDBCon(DataSource dataSource) {
+		return arg -> {
+			try {
+				dataSource.getConnection().close();
+				logger.info("^^^^^ 연결성공 ^^^^^^");
+			} catch (Exception e) {
+				logger.error("ㅠㅠㅠㅠㅠㅠ 연결실패 ㅠㅠㅠㅠㅠㅠ");
+			}
+		};
+	}
+}
